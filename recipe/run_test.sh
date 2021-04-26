911dd3c300c69a63487031689feb5c402de8ebf6
Jason Furmanek <furmanek@us.ibm.com>
2021-04-26 11:27:04 -0500

    initial checkin v1.29.1

---
+++ recipe/run_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ex
+
+# Compile a trivial service definition to C++
+
+protoc -I$RECIPE_DIR --plugin=protoc-gen-grpc=$PREFIX/bin/grpc_cpp_plugin --grpc_out=. hello.proto
+
+test -f hello.grpc.pb.h
+test -f hello.grpc.pb.cc
+
+test -f $PREFIX/lib/libgrpc${SHLIB_EXT}
+test -f $PREFIX/lib/libgrpc_unsecure${SHLIB_EXT}
+test -f $PREFIX/lib/libgrpc++${SHLIB_EXT}
+test -f $PREFIX/lib/libgrpc++_unsecure${SHLIB_EXT}
